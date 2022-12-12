d75b6ad8ed7e25726da4a5abb216b345b9c847d3
Alanthomas-2209 <2209@tkmce.ac.in>
2022-12-12 11:07:55 +0530

    added

--- CO2/q11.py
+++ CO2/q11.py
@@ -1,14 +1,14 @@
-s=int(input("Enter the side length of square :"))
-areas=lambda x :x*x
-print("Area =",areas(s))
+s = int(input("Enter the length of side of a square :"))
+areas = lambda x: x * x
+print("Area =", areas(s))
 
-l=int(input("Enter the  length of rectangle :"))
-b=int(input("Enter the breadth of rectangle :"))
-arear= lambda q,w :q*w
-print("Area =",arear(l,b))
+l = int(input("Enter the  length of rectangle :"))
+b = int(input("Enter the breadth of rectangle :"))
+arear = lambda q, w: q * w
+print("Area =", arear(l, b))
 
-ba=int(input("Enter the base of the triangle :"))
-he=int(input("Enter the height of the triangle :"))
+ba = int(input("Enter the base of the triangle :"))
+he = int(input("Enter the height of the triangle :"))
 
-areat= lambda t,u:.5*t*u
-print("Area =",areat(ba,he))
+areat = lambda t, u: .5 * t * u
+print("Area =", areat(ba, he))
---
+++ CO4/Q1/main.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def Area(self, length, breadth):
+        area = self.length * self.breadth
+        return area
+
+    def Perimeter(self, length, breadth):
+        perimeter = 2 * (self.length + self.breadth)
+        return perimeter
+
+
