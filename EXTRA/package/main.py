96cc806712a8edfcd5c5a8b5316bc642df4b2424
Alanthomas-2209 <2209@tkmce.ac.in>
2022-12-19 11:14:24 +0530

    CO3Q1

--- EXTRA/package/main.py
+++ EXTRA/package/main.py
@@ -1,9 +1,10 @@
-from arthimetic import *
-'''from arthimetic import product
-from arthimetic.quotient import quotient'''
+from arthimetic import sum, product, quotient, diff
+
 a = sum(10, 5)
 print(a)
 a = product(10, 5)
 print(a)
 a = quotient(10, 5)
+print(a)
+a = diff(10, 5)
 print(a)
