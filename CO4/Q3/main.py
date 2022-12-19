43f854e3b93868265ccd1b84927bd70ff1756754
Alanthomas-2209 <2209@tkmce.ac.in>
2022-12-19 15:09:44 +0530

    co4q4 completed

--- CO4/Q3/main.py
+++ CO4/Q3/main.py
@@ -12,5 +12,17 @@ class compare:
         return self.area < obj.area
 
 
-rect1 = compare(5,6)
+length1 = int(input("Enter the length of the first rectangle :"))
+breadth1 = int(input("Enter the breadth of the first rectangle :"))
+rect1 = compare(length1, breadth1)
 print(rect1.__str__())
+
+length2 = int(input("Enter the length of the second rectangle :"))
+breadth2 = int(input("Enter the breadth of the second rectangle :"))
+rect2 = compare(length2, breadth2)
+print(rect2.__str__())
+
+if rect1 < rect2:
+    print("Area of second rectangle is greater")
+else:
+    print("Area of first rectangle is greater")
