ebc93b7312b295c370d96ec265c116fff15b60ac
Alanthomas-2209 <2209@tkmce.ac.in>
2023-01-16 12:15:11 +0530

    some minor fixes

--- CO5/Q3/Q3.py
+++ CO5/Q3/Q3.py
@@ -4,24 +4,49 @@ import os
 
 path = os.getcwd()
 
+fieldnames = ['Programming language','Designed by','Appeared','Extension']
+data = [
+    {'Programming language':'Python','Designed by':'Guido van Rossum', 'Appeared':'1991', 'Extension':'.py'},
+    {'Programming language':'Python','Designed by':'Guido van Rossum', 'Appeared':'1991', 'Extension':'.py'},
+    {'Programming language':'Python','Designed by':'Guido van Rossum', 'Appeared':'1991', 'Extension':'.py'}
+    
+]
+f = open("text.csv", 'w')
+fileWriter = csv.DictWriter(f,fieldnames= fieldnames,delimiter=",")
+fileWriter.writeheader()
+fileWriter.writerows(data)
+f.close()
+
+
+with open('text.csv') as file:
+    fileReader = csv.reader(file)
+    for line in fileReader:
+        print(line)
+
+# f = open('text.csv','r')
+# fileReader = csv.DictReader(f)
+# for line in fileReader:
+#     print(line['Appeared'])
+
+
+
 # C = {'Programming language': ['Python','Java', 'C++'],
 #         'Designed by': ['Guido van Rossum', 'James Gosling', 'Bjarne Stroustrup'],
 #         'Appeared': ['1991', '1995', '1985'],
 #         'Extension': ['.py', '.java', '.cpp'],
 #     }
-# path = getcwd()
 # df = DataFrame(C, columns= ['Programming language', 'Designed by', 'Appeared', 'Extension'])
-# export_csv = df.to_csv (r'C:\Users\Admin\Desktop\CODE\Alan\PROGRAMMING-LAB\CO5\Q3\pandaresult.csv', index = None, header=True) # here you have to write path, where result file will be stored
+# export_csv = df.to_csv (r,path + '\pandaresult.csv', index = None, header=True) # here you have to write path, where result file will be stored
 # print(df)
 
-with open(path + '\writeData.csv', mode='w') as file:
-    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-    # way to write to csv file
-    writer.writerow(['Programming language', 'Designed by', 'Appeared', 'Extension'])
-    writer.writerow(['Python', 'Guido van Rossum', '1991', '.py'])
-    writer.writerow(['Java', 'James Gosling', '1995', '.java'])
-    writer.writerow(['C++', 'Bjarne Stroustrup', '1985', '.cpp'])
-
-result = pandas.read_csv(path + '\writeData.csv')
-print(result)
+# with open(path + '\writeData.csv', mode='w') as file:
+#     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#
+#     # way to write to csv file
+#     writer.writerow(['Programming language', 'Designed by', 'Appeared', 'Extension'])
+#     writer.writerow(['Python', 'Guido van Rossum', '1991', '.py'])
+#     writer.writerow(['Java', 'James Gosling', '1995', '.java'])
+#     writer.writerow(['C++', 'Bjarne Stroustrup', '1985', '.cpp'])
+#
+# result = pandas.read_csv(path + '\writeData.csv')
+# print(result)
--- CO5/Q4/main.py
+++ CO5/Q4/main.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 # specify the column names to be read from the CSV file
 # columns = ['column1', 'column2']
-#
-# # read the CSV file and print the contents of the selected columns
-# with open('file.csv', 'r') as csvfile:
-#     reader = csv.DictReader(csvfile)
-#     for row in reader:
-#         print([row[col] for col in columns])
-
-# specify the column names to be read from the CSV file
-columns = ['column1', 'column2']
 
 # read the CSV file and print the contents of the selected columns
-df = pd.read_csv('file.csv', usecols=columns)
-print(df)
+with open('file.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        # print([row[col] for col in columns])
+        print(row[1])
+
+# # specify the column names to be read from the CSV file
+# columns = ['column1', 'column2']
+
+# # read the CSV file and print the contents of the selected columns
+# df = pd.read_csv('file.csv', usecols=columns)
+# print(df)
