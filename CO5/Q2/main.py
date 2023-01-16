db0b651d2f19ffc37080bae0eb8d7a7e049e12cb
Alanthomas-2209 <2209@tkmce.ac.in>
2023-01-17 04:17:30 +0530

    idk

--- CO5/Q2/main.py
+++ CO5/Q2/main.py
@@ -2,7 +2,6 @@ file1 = open('./football.txt', 'r')
 file2 = open('file2.txt', 'w')
 
 lines = file1.readlines()
-type(lines)
 for i in range(0, len(lines)):
     if i % 2 == 0: # array starts from 0
         file2.write(lines[i])
