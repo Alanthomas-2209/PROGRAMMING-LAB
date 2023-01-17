7ae9a8df2423c86709f2f9789209863b5307b973
Alanthomas-2209 <2209@tkmce.ac.in>
2023-01-18 00:26:52 +0530

    added different type

--- CO1/q4.py
+++ CO1/q4.py
@@ -1,5 +1,12 @@
-
 from collections import Counter
 string=input("Enter a string :")
 list=string.split()
 print(Counter(list))
+
+# string=input("Enter the string")
+# unique=set(string.split())
+# words=string.split()
+# count={}
+# for word in unique:
+#     count[word]=words.count(word)
+# print(count)
